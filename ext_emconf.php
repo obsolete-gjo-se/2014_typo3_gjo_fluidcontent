8c0984eb50215b448a49a878ad9e6c89b9c11b7d
Gregory Jo <gregory.jo@gjo-se.com>
2014-06-09 18:07:20 +0200

    Übernahme aktueller Produktiv-Stand

---
+++ ext_emconf.php
@@ -0,0 +1,43 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "gjo_fluidcontent".
+ ****************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Gjo Provider für Content: Twitter Bootstrap Elements',
+    'description' => 'A collection of Twitter Bootstrap oriented Fluid Elements',
+    'category' => 'misc',
+    'shy' => 0,
+    'version' => '0.0.1',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'test',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 1,
+    'lockType' => '',
+    'author' => 'gregory jo',
+    'author_email' => 'gregory.jo@gjo-se-com',
+    'author_company' => 'gjo-se.com',
+    'constraints' =>
+    array(
+        'depends' =>
+        array(
+            'typo3' => '6.1.00-6.2.99',
+            'cms' => '',
+            'fluidcontent' => '',
+            'vhs' => '',
+        ),
+        'conflicts' =>
+        array(),
+        'suggests' =>
+        array(),
+    ),
+);
+
+?>
